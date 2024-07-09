33a758fe14d0286fdaf613958aeaec29c4de9c8d
ageudm <ageudm@outlook.com>
2024-07-10 00:33:07 +0100

    imprtação e expolrtação de objectos

---
+++ 4-object-impport.js
@@ -0,0 +1,12 @@
+//para importar objecto podemos usar os mesmo metodos require e exports
+
+//ex de array
+module.exports.listFruts = ['laranja', 'limão'];
+
+//ex de objecto
+const person = {
+    name: 'Ageu Miguel',
+    idade: 31
+}
+
+module.exports.singlePerson = person;
