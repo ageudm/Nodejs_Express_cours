7a65c2dc72575d02cbf4261c3f819dcdfcd6a647
ageudm <ageudm@outlook.com>
2024-07-11 23:42:21 +0100

    impotar dados sem e executar sem ser expotado

--- 3-utilits.js
+++ 3-utilits.js
@@ -6,4 +6,9 @@ const soma = (num1, num2) => {
     console.log(num1 + num2);
 }
 
-module.exports = sayHelo, soma;
+//dados excutados
+const lerIdade = (num) => {
+    console.log(`a minha idade é: ${num}`);
+}; lerIdade(20)
+
+module.exports = sayHelo, soma; //para exporatação de fução os elementos devem ser separado com virgulas
--- app.js
+++ app.js
@@ -16,9 +16,12 @@
 const names = require('./2-variables');
 const sayHelo = require('./3-utilits');
 const soma = require('./3-utilits');
+const person = require('./4-object-impport')
 
+require('./3-utilits') //executar os dados de outro documento sem expotar
 
 
 
 sayHelo(names.miguel)
 soma(20, 30);
+console.log(person.singlePerson);
